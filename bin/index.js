4cba5fc5571ebe437d2c0f01e6ac5f6129e868f6
simov <simeonvelichkov@gmail.com>
2017-02-18 20:22:39 +0200

    Major refactoring

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,33 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2))
+
+if (argv.help) {
+  console.log('--env development|staging|production')
+  console.log('--config path/to/config.json')
+  console.log('--db path/to/db.json')
+  console.log('--quotes path/to/quotes.json')
+  process.exit()
+}
+
+if (!argv.config) {
+  console.error('Specify --config file')
+  process.exit()
+}
+
+if (!argv.db) {
+  console.error('Specify --db file')
+  process.exit()
+}
+
+if (!argv.quotes) {
+  console.error('Specify --quotes file')
+  process.exit()
+}
 
 var hook = require('../')
 
+hook.init()
 hook.check()
   .then((responses) => responses && responses.forEach(([res, body]) =>
     console.log(new Date().toString(), res.statusCode, body)
