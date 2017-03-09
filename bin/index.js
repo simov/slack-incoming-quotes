99ef634fde1a31edd7c07ba72de30be2a2717597
simov <simeonvelichkov@gmail.com>
2017-03-09 12:03:02 +0200

    Major refactoring

--- bin/index.js
+++ bin/index.js
@@ -3,34 +3,40 @@
 var argv = require('minimist')(process.argv.slice(2))
 
 if (argv.help) {
-  console.log('--env development|staging|production')
+  console.log('--env environment')
   console.log('--config path/to/config.json')
   console.log('--db path/to/db.json')
   console.log('--quotes path/to/quotes.json')
   process.exit()
 }
 
-if (!argv.config) {
-  console.error('Specify --config file')
-  process.exit()
-}
+;['config', 'db', 'quotes'].forEach((key) => {
+  if (!argv[key]) {
+    console.error(`Specify --${key} path/to/config.json`)
+    process.exit()
+  }
+})
 
-if (!argv.db) {
-  console.error('Specify --db file')
-  process.exit()
-}
 
-if (!argv.quotes) {
-  console.error('Specify --quotes file')
-  process.exit()
-}
+var path = require('path')
+
+
+var env = process.env.NODE_ENV || argv.env || 'development'
+
+var config = require(path.resolve(process.cwd(), argv.config))[env]
+
+var dbpath = path.resolve(process.cwd(), argv.db)
+var db = require(dbpath)
+
+var quotes = require(path.resolve(process.cwd(), argv.quotes))
+
 
 var hook = require('../')
 
-hook.init()
-hook.check()
-  .then((responses) => responses && responses.forEach(([res, body]) =>
-    console.log(new Date().toString(), res.statusCode, body)
-  ))
-  .catch((err) =>
-    console.error(new Date().toString(), err))
+hook({env, config, db, dbpath, quotes})
+  .then((responses) => {
+    responses.forEach(([res, body]) => {
+      console.log(new Date().toString(), res.statusCode, body)
+    })
+  })
+  .catch((err) => console.error(new Date().toString(), err))
